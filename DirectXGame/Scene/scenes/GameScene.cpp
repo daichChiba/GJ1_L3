f0c2ab3b345e814f10732367719ffc36b1f01b49
daichChiba <146914435+daichChiba@users.noreply.github.com.>
2025-09-02 21:24:52 +0900

    Sceneの追加、GameSceneの追加、ResetSceneの追加

---
+++ DirectXGame/Scene/scenes/GameScene.cpp
@@ -0,0 +1,72 @@
+#include "GameScene.h"
+
+using namespace KamataEngine;
+GameScene::GameScene() {
+
+}
+
+GameScene::~GameScene() {
+
+}
+
+void GameScene::Initialize() {
+
+}
+
+void GameScene::Update() {
+
+}
+
+void GameScene::Draw() {
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw();
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw();
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+
+
+
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw();
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
+
+void GameScene::Delete() {
+
+}
+
+void GameScene::DrawImGui() {
+	ImGui::Begin("GameScene");
+	ImGui::Text("Test");
+	ImGui::Checkbox("isFinished", &isFinish);
+	ImGui::End();
+}
--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,57 @@
 #include <Windows.h>
+#include <KamataEngine.h>
+#include <map>
 
+using namespace KamataEngine;
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	
+	// エンジンの初期化
+	Initialize(L"3048");
+
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+
+
+#pragma region 汎用機能初期化
+	// ImGuiManagerインスタンスの取得
+	ImGuiManager* imguiManager_ = ImGuiManager::GetInstance();
+#pragma endregion
+	// メインループ
+	while (true) {
+		// エンジンの更新
+		if (Update()) {
+			break;
+		}
+
+		if (Input::GetInstance()->PushKey(DIK_ESCAPE)) {
+			break;
+		}
+
+
+		// ImGui受付開始
+		imguiManager_->Begin();
+
+
+		// ImGui受付開始
+		imguiManager_->End();
+
+		// 描画開始
+		dxCommon->PreDraw();
+
+		// ここに描画処理を記述する
+
+		// ImGui描画
+		imguiManager_->Draw();
+
+		// 描画終了
+		dxCommon->PostDraw();
+
+	}
+
+
+	// エンジンの終了処理
+	Finalize();
+
 	return 0;
 }
