936fd1ed29e4d30e36e502efc9eb8a0da15e7251
daichChiba <146914435+daichChiba@users.noreply.github.com.>
2025-09-03 11:25:17 +0900

    sceneManagerは作っている途中

---
+++ DirectXGame/Scene/manager/SceneManager.cpp
@@ -0,0 +1,13 @@
+#include "SceneManager.h"
+#include "Scene/scenes/GameScene.h"
+#include "Scene/scenes/ResetScene.h"
+
+class SceneManager {
+public:
+	void Update();
+	void Draw();
+	void DrawImGui();
+
+private:
+
+};
---
+++ DirectXGame/Scene/manager/SceneManager.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Scene/scenes/GameScene.h"
+#include "Scene/scenes/ResetScene.h"
+#include <KamataEngine.h>
+#include <map>
+class SceneManager {
+public:
+
+};
